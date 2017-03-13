a8eae311655a6c70918689866246c3fcb7765d54
elhajjaji <elhajjajihamza@gmail.com>
2017-03-13 19:53:31 +0000

    update public folder

---
+++ resources/views/index.blade.php
@@ -0,0 +1,182 @@
+	<?xml version="1.0" encoding="utf-8"?>
+
+	<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+
+	 "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+
+
+	<html dir="rtl" lang="ar"  xml:lang="ar">
+
+	<head>
+
+	 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+			<meta name="viewport" content="width=device-width, initial-scale=1.0">
+			<meta name="description" content="">
+			<meta name="author" content="">
+
+
+			<title>الملحقية الثقافية الليبية بالمغرب</title>
+
+			<!-- Bootstrap core CSS -->
+			<link href="css/bootstrap.css" rel="stylesheet">
+			<link href="css/custom-styles.css" rel="stylesheet">
+			<link href="css/image-effects.css" rel="stylesheet">
+
+			 <link href="css/normalize.css" rel="stylesheet">
+			 <link href="css/component.css" rel="stylesheet">
+				<link rel="stylesheet" href="css/font-awesome.css">
+				<link rel="stylesheet" href="css/font-awesome-ie7.css">
+
+			<script src="js/html5shiv.js"></script>
+				<script src="js/respond.min.js"></script>
+			<script src="js/modernizr.custom.js"></script>
+
+		</head>
+
+		<body>
+			 <div class="container page-styling">
+			<div class="header-wrapper">
+				<div class="site-name">
+					<h1>الملحقية الثقافية الليبية بالمغرب</h1>
+				</div>
+				<div class="menu">
+					<div class="navbar">
+
+							<div class="navbar-header">
+								<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+									<span class="icon-bar"></span>
+									<span class="icon-bar"></span>
+									<span class="icon-bar"></span>
+								</button>
+							</div>
+							<div class="navbar-collapse collapse">
+								<ul class="nav navbar-nav ">
+									
+									<li class="dropdown ">
+								<a href="#" class="dropdown-toggle active" data-toggle="dropdown">عن الملحقية <b class="caret"></b></a>
+								<ul class="dropdown-menu">
+									<li ><a href="#">مقدمة</a></li>
+								</ul>
+							</li>
+									<li class="dropdown ">
+								<a href="#" class="dropdown-toggle active" data-toggle="dropdown"> العلاقات الأكاديمية <b class="caret"></b></a>
+								<ul  class="dropdown-menu">
+									<li><a href="#">مقدمة</a></li>
+								</ul>
+							</li>
+									<li><a href="#">التعليم في المغرب</a></li>
+									<li><a href="#">عنوان الملحقية</a></li>
+									<li><a href="#">الرئيسية</a></li>	
+								</ul>
+							
+							</div><!--/.navbar-collapse -->
+
+						</div>
+						<div class="mini-menu">
+							<label>
+						<select class="selectnav">
+							<option value="#" selected="">الرئيسية</option>
+							<option value="#">About</option>
+							<option value="#">→ Another action</option>
+							<option value="#">→ Something else here</option>
+							<option value="#">→ Another action</option>
+							<option value="#">→ Something else here</option>
+							<option value="#">Services</option>
+							<option value="#">Portfolio</option>
+							<option value="#">Contact</option>
+						</select>
+						</label>
+						</div>
+			</div>
+				</div>
+				<div class="banner">
+						<div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
+		<!-- Wrapper for slides -->
+		<div class="carousel-inner">
+			<div class="item active">
+				<img src="img/slider-4.jpg" alt="" class="">
+				<div class="carousel-caption">
+						<a href="#" class="btn"> عنوان الخبر :اضغط لقراءة المزيد </a>
+				</div>
+			</div>
+			<div class="item">
+				<img src="img/slider-5.jpg" alt="">
+				<div class="carousel-caption">
+					<a href="#" class="btn"> عنوان الخبر :اضغط لقراءة المزيد </a>
+				</div>
+			</div>
+			<div class="item">
+				<img src="img/slider-6.jpg" alt="">
+				<div class="carousel-caption">
+						<a href="#" class="btn"> عنوان الخبر :اضغط لقراءة المزيد </a>
+				</div>
+			</div>
+		</div>
+
+		<!-- Controls -->
+		<a class="left carousel-control" href="#carousel-example-generic" data-slide="prev">
+			<span class="glyphicon glyphicon-chevron-left"></span>
+		</a>
+		<a class="right carousel-control" href="#carousel-example-generic" data-slide="next">
+			<span class="glyphicon glyphicon-chevron-right"></span>
+		</a>
+	</div>
+				</div>
+				<div class="content-wrap">
+					<div class="main-title">
+						<ul class="grid effect-8" id="grid">
+							<li	><h1>كلمة السفير</h1>
+						<h4>السلام عليكم...<br>
+
+	أعزائي الطلبة الدارسين بالساحة المغربية أسعد الله
+	أوقاتكم أينما كنتم, من خلال هذه الصفحة الخاصة بالملحقية الثقافية بالرباط, أحييكم و أتمنى أن تكو
+	ن جسر التواصل بينكم و بين المسؤولين بالملحقية لتدليل
+	كافة الصعاب و المشاكل التي تعترضكم, متمنيا من الله العلي القدير أن يوفقكم إلى النجاح و رفع راية ليبيا عاليا في مختلف المحافل العلمية.
+	<br><br>
+	السفير عبد المجيد غيث سيف النصر القائم بالأعمال</h4></li>
+						</ul>
+
+					</div>
+				</div>
+				
+		
+			
+		
+
+			</div><!-- /container -->
+
+			<div class="container">
+		<div dir="ltr" class="copy-rights">
+			Copyright(c) 2017 culturalAffairs.<br>
+		</div>
+		</div>
+
+
+
+
+
+
+
+			<!-- Bootstrap core JavaScript
+			================================================== -->
+			<!-- Placed at the end of the document so the pages load faster -->
+			<script src="js/jquery-1.9.1.js"></script>
+			<script src="js/bootstrap.js"></script>
+			<script src="js/modernizr-2.6.2-respond-1.1.0.min.js"></script>
+			 <script src="js/masonry.pkgd.min.js"></script>
+			<script src="js/imagesloaded.js"></script>
+			<script src="js/classie.js"></script>
+			<script src="js/AnimOnScroll.js"></script>
+
+			<script>
+				new AnimOnScroll( document.getElementById( 'grid' ), {
+					minDuration : 0.4,
+					maxDuration : 0.7,
+					viewportFactor : 0.2
+				} );
+			</script>
+
+
+		</body>
+	</html>
